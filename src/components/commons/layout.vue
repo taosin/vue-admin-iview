51e0eeeef241f7081b8bed20a0cf3e51735b7105
iMocco <taoxin167@gmail.com>
2018-07-24 17:21:47 +0800

    create layout

---
+++ src/components/commons/layout.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="layout">
+		<div slot="left">
+			滴撒奥所大多
+		</div>
+	</div>
+</template>
+<script>
+export default{
+	name: 'layout',
+	data () {
+		return {
+			name: '233132'
+		}
+	}
+}
+</script>
+
+<style scoped lang="less">
+</style>
--- src/views/dashboard.vue
+++ src/views/dashboard.vue
@@ -7,12 +7,21 @@
 		<div>
 			<Page :current="1" :total="100"></Page>
 		</div>
+		<layout>
+			<slot name="left">
+				大大大
+			</slot>
+		</layout>
 	</div>
 </template>
 
 <script>
+import layout from './../components/commons/layout.vue'
 export default {
 	name: 'dashboard',
+	components: {
+		'layout': layout
+	},
 	data () {
 		return {
 			title: '欢迎回来'
