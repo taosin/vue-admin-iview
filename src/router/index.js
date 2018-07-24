835bca64d33b2bc853e755a3f802dd0de072a284
iMocco <taoxin167@gmail.com>
2018-07-24 16:36:53 +0800

    finish project init

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-
+import Dashboard from '@/views/dashboard'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Dashboard',
+      component: Dashboard
     }
   ]
 })
---
+++ src/views/dashboard.vue
@@ -0,0 +1,19 @@
+<template>
+	<div>
+		<p>首页</p>
+		<h1>
+			{{title}}
+		</h1>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'dashboard',
+	data () {
+		return {
+			title: '欢迎回来'
+		}
+	}
+}
+</script>
