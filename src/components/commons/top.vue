eeb7d685791994f8a46d79e44dbbe120fe53ceb4
iMocco <taoxin167@gmail.com>
2018-07-28 18:24:29 +0800

    modify top

--- src/components/commons/top.vue
+++ src/components/commons/top.vue
@@ -5,8 +5,20 @@
 		</div>
 		<div class="top-right">
 			<div class="user-info">
-				eqeq
-				<img src="http://blog.gdfengshuo.com/example/work/static/img/img.jpg" alt="">
+				<Badge count="3" class="badge">
+					<Icon type="ios-bell-outline" size="30"></Icon>
+				</Badge>
+				<Avatar src="http://blog.gdfengshuo.com/example/work/static/img/img.jpg" class="avatar" size="large"/>
+				<Dropdown class="name">
+					<a>
+						admin
+						<Icon type="arrow-down-b"></Icon>
+					</a>
+					<DropdownMenu slot="list">
+						<DropdownItem>个人信息</DropdownItem>
+						<DropdownItem :on-click="logout">退出</DropdownItem>
+					</DropdownMenu>
+				</Dropdown>
 			</div>
 		</div>
 	</div>
@@ -18,22 +30,40 @@ export default{
 		return {
 			name: '233132'
 		}
+	},
+	methods: {
+		logout () {
+			debugger
+			this.$router.push('/login')
+		}
 	}
 }
 </script>
 
 <style scoped lang="less">
 .layout{
-	padding-left:20px;
+	padding: 0 20px;
 	line-height: 80px;
 	.top-left{
-		border: 1px solid;
-		width: 200px;
+		width: 220px;
+		float:left;
 	}
 	.top-right{
 		float: right;
 		width:400px;
-		background: red;
+		text-align: right;
+		padding-right:20px;
+		.user-info{
+			.avatar{
+				margin:0 20px;
+			}
+			.name{
+				float: right;
+			}
+			.badge{
+				margin-top: 10px;
+			}
+		}
 	}
 }
 </style>
