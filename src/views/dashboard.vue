bb196358f6a9417d5b9860fdbd92ce9405776119
iMocco <taoxin167@gmail.com>
2018-07-29 22:23:27 +0800

    modify dashboard.scss

--- src/views/dashboard.vue
+++ src/views/dashboard.vue
@@ -1,50 +1,60 @@
 <template>
 	<div class="dashboard">
+
+		<!-- 展示台 -->
 		<div class="today-counts">
 			<Row :gutter="30">
 				<Col span="6">
-					<div class="count-box">
-						<div class="left eye">
+					<div class="box eye">
+						<div class="left">
 							<Icon type="eye" size="64"></Icon>
 						</div>
-						<div class="right eye-text">
+						<div class="right">
 							<h2>123334</h2>
 							<p>用户访问量</p>
 						</div>
 					</div>
 				</Col>
 				<Col span="6">
-					<div class="count-box">
-						<div class="left message">
+					<div class="box message">
+						<div class="left">
 							<Icon type="chatbubble-working" size="64"></Icon>
 						</div>
-						<div class="right message-text">
+						<div class="right">
 							<h2>123334</h2>
 							<p>系统消息</p>
 						</div>
 					</div>
 				</Col>
 				<Col span="6">
-					<div class="count-box">
-						<div class="left order">
+					<div class="box order">
+						<div class="left">
 							<Icon type="bag" size="64"></Icon>
 						</div>
-						<div class="right order-text">
+						<div class="right">
 							<h2>123334</h2>
 							<p>今日订单量</p>
 						</div>
 					</div>
 				</Col>
 				<Col span="6">
-					<div class="count-box">
-						<div class="left cash">
+					<div class="box cash">
+						<div class="left">
 							<Icon type="social-yen" size="64"></Icon>
 						</div>
-						<div class="right cash-text">
+						<div class="right">
 							<h2>123334</h2>
 							<p>今日交易额</p>
 						</div>
 					</div>
+				</Col>
+			</Row>
+		</div>
+
+		<div class="middle-content">
+			<Row :gutter="30">
+				<Col span="12">
+					div.content-box
 				</Col>
 			</Row>
 		</div>
