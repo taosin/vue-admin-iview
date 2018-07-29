8f1552703df633cede0d6fcc74ca6e06e7c90c61
iMocco <taoxin167@gmail.com>
2018-07-29 22:08:48 +0800

    modify dashboard

--- src/views/dashboard.vue
+++ src/views/dashboard.vue
@@ -1,5 +1,53 @@
 <template>
 	<div class="dashboard">
+		<div class="today-counts">
+			<Row :gutter="30">
+				<Col span="6">
+					<div class="count-box">
+						<div class="left eye">
+							<Icon type="eye" size="64"></Icon>
+						</div>
+						<div class="right eye-text">
+							<h2>123334</h2>
+							<p>用户访问量</p>
+						</div>
+					</div>
+				</Col>
+				<Col span="6">
+					<div class="count-box">
+						<div class="left message">
+							<Icon type="chatbubble-working" size="64"></Icon>
+						</div>
+						<div class="right message-text">
+							<h2>123334</h2>
+							<p>系统消息</p>
+						</div>
+					</div>
+				</Col>
+				<Col span="6">
+					<div class="count-box">
+						<div class="left order">
+							<Icon type="bag" size="64"></Icon>
+						</div>
+						<div class="right order-text">
+							<h2>123334</h2>
+							<p>今日订单量</p>
+						</div>
+					</div>
+				</Col>
+				<Col span="6">
+					<div class="count-box">
+						<div class="left cash">
+							<Icon type="social-yen" size="64"></Icon>
+						</div>
+						<div class="right cash-text">
+							<h2>123334</h2>
+							<p>今日交易额</p>
+						</div>
+					</div>
+				</Col>
+			</Row>
+		</div>
 	</div>
 </template>
 
