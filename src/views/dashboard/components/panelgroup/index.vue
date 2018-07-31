a6e4d24162a498b199730178fa137d86af4bf3cf
iMocco <taoxin167@gmail.com>
2018-07-31 20:05:47 +0800

    将页面组件化

---
+++ src/views/dashboard/components/panelgroup/index.vue
@@ -0,0 +1,60 @@
+<template>
+	<div style="width:100%">
+		<Row :gutter="30">
+			<Col span="6">
+			<div class="box eye">
+				<div class="left">
+					<Icon type="eye" size="64"></Icon>
+				</div>
+				<div class="right">
+					<h2>123334</h2>
+					<p>用户访问量</p>
+				</div>
+			</div>
+		</Col>
+		<Col span="6">
+		<div class="box message">
+			<div class="left">
+				<Icon type="chatbubble-working" size="64"></Icon>
+			</div>
+			<div class="right">
+				<h2>123334</h2>
+				<p>系统消息</p>
+			</div>
+		</div>
+	</Col>
+	<Col span="6">
+	<div class="box order">
+		<div class="left">
+			<Icon type="bag" size="64"></Icon>
+		</div>
+		<div class="right">
+			<h2>123334</h2>
+			<p>今日订单量</p>
+		</div>
+	</div>
+</Col>
+<Col span="6">
+<div class="box cash">
+	<div class="left">
+		<Icon type="social-yen" size="64"></Icon>
+	</div>
+	<div class="right">
+		<h2>123334</h2>
+		<p>今日交易额</p>
+	</div>
+</div>
+</Col>
+</Row>
+</div>
+</template>
+<script>
+export default{
+	name: 'panel-group',
+	data () {
+		return {
+
+		}
+	}
+}
+</script>
