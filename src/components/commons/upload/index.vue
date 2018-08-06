4d82b31b98a3e5459a60aaab5a6f9dfc10cfd826
iMocco <taoxin167@gmail.com>
2018-08-06 11:37:19 +0800

    add upload

---
+++ src/components/commons/upload/index.vue
@@ -0,0 +1,29 @@
+<template>
+	<div>
+		<Upload action="//jsonplaceholder.typicode.com/posts/" :multiple="multiple" :type="type">
+			<Button type="ghost" icon="ios-cloud-upload-outline">Upload files</Button>
+		</Upload>
+	</div>
+</template>
+<script>
+export default{
+	props: {
+		multiple: {
+			type: Boolean,
+			default: false
+		},
+		draggable:{
+			type: Boolean,
+			default: false
+		}
+	},
+	data () {
+		return {
+			type: this.draggable ? 'drag' : 'select'
+		}
+	},
+	methods: {
+
+	}
+}
+</script>
