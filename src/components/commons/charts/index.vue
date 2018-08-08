96037bbdb6a776a110c9eede57dabb0cc8b9e9b6
iMocco <taoxin167@gmail.com>
2018-08-08 11:08:31 +0800

    add charts components

---
+++ src/components/commons/charts/index.vue
@@ -0,0 +1,35 @@
+<template>
+	<div>
+		<canvas :ref="ref" style="width:100%;height:100%"></canvas>
+	</div>
+</template>
+<script>
+const Chart = require('chart.js')
+export default{
+	props: {
+		ref: {
+			Type: String,
+			default: 'chartCavans'
+		},
+		opt: {
+			type: Object,
+			default : {}
+		}
+	},
+	data () {
+		return {
+
+		}
+	},
+	methods: {
+		init () {
+			const self = this
+			let cavans = self.$refs.ref
+			let ctx = canvas.getContext('2d')
+			let myChart = new Chart(ctx, {
+
+			})
+		}
+	}
+}
+</script>
