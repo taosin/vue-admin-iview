64c77789d309f03bc28a086387095edff87172d1
iMocco <taoxin167@gmail.com>
2018-08-09 17:17:41 +0800

    modify charts

--- src/components/commons/charts/index.vue
+++ src/components/commons/charts/index.vue
@@ -1,35 +1,60 @@
 <template>
-	<div>
-		<canvas :ref="ref" style="width:100%;height:100%"></canvas>
+	<div class="chart">
+		<canvas :ref="chartRef" style="width:100%;height:100%"></canvas>
 	</div>
 </template>
 <script>
 const Chart = require('chart.js')
 export default{
 	props: {
-		ref: {
+		chartRef: {
 			Type: String,
 			default: 'chartCavans'
 		},
-		opt: {
+		type: {
+			Type: String,
+			default: 'bar'
+		},
+		data: {
 			type: Object,
-			default : {}
+			default: () => {
+				return {
+					labels: [],
+					datasets: []
+				}
+			}
+		},
+		options: {
+			type: Object,
+			default: () => {
+				return {}
+			}
 		}
 	},
 	data () {
 		return {
-
 		}
 	},
 	methods: {
 		init () {
 			const self = this
-			let cavans = self.$refs.ref
-			let ctx = canvas.getContext('2d')
-			let myChart = new Chart(ctx, {
-
+			let cavans = self.$refs[self.chartRef]
+			let ctx = cavans.getContext('2d')
+			var chart = new Chart(ctx, {
+				type: self.type,
+				data: self.data,
+				options: self.options
 			})
 		}
+	},
+	mounted(){
+		this.init()
 	}
 }
 </script>
+<style>
+	.chart{
+		width: 100%;
+		height: 100%;
+	}
+</style>
